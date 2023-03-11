6fbd5cdee2545767264e27f651a842254ddb02cc
Your Name <you@example.com>
2023-03-11 12:37:27 +0200

    made alot of changes to UI

---
+++ Portfolio/index.js
@@ -0,0 +1,24 @@
+
+
+
+
+function hoverchanges(){
+   document.getElementById("heading").style.display='none'; 
+ 
+
+}
+function hoverchangesTwo(){
+   document.getElementById("heading").style.display='flex'; 
+
+ 
+ }
+ function hoverchanges1(){
+
+
+  document.getElementById("heading1").style.display='none'; 
+}
+function hoverchangesTwo1(){
+  document.getElementById("heading1").style.display='flex'; 
+
+
+}
